c8b460ac6898f35a94e7636fd9c722176f3fbc1f
Vetch101 <vetch101@googlemail.com>
2015-06-04 10:54:39 +0100

    Added comments to plot1.R explaining how to derive the appropriate skip and nrow values

--- plot1.R
+++ plot1.R
@@ -10,15 +10,39 @@ loadData <- function() {
   # Load data
   filename <- "household_power_consumption.txt"
   header <- read.csv2(filename, header = TRUE, nrow = 1)
+  
+  # Identify skip and nrow values in advance using:  
+  # first = grep("^[12]/2/2007", readLines(filename))[1]
+  # last = grep("^3/2/2007", readLines(filename))[1]
+  # skip = first - 1
+  # nrow = last - first
+  # closeAllConnections()
+  
   data <- read.csv2(filename, header = FALSE,
                     stringsAsFactors=FALSE, skip = 66637, nrow = 2880)
+  
+  # Set names to names based on the header
+  
   names(data) <- names(header)
+  
+  # Combine Date and Time columns into DateTime column
+  
   data <- cbind(DateTime = paste(data$Date, data$Time), data)
+  
+  # Convert DateTime to POSIXct
+  
   data$DateTime <- as.POSIXct(data$DateTime, format = "%d/%m/%Y %H:%M:%S")
+  
+  # Drop original Date and Time columns
+  
   data <- data[ , c(1,4:10)]
+  
+  # Set all other columns to numeric
+  
   data[,2:8] <- sapply(data[,2:8], as.numeric)  
 
   # Return dataset
+  
   return(data)
   
 }
